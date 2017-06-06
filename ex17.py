a2f8d605051fcabbc82889b27219c54880774874
Beth Tenorio <betsky98@gmail.com>
2017-06-05 23:57:55 -0400

    wip - handle ones, tens and multiples of hundreds TODO: handle tens

--- ex17.py
+++ ex17.py
@@ -7,11 +7,54 @@ def spellout(num):
     converter = {
         0: 'zero',
         1: 'one',
-        1000: 'one thousand'
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'fourty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+        100: 'hundred'
     }
-    return converter.get(num, 'out of range')
+    return converter.get(num, 'not found')
 
 def count_letters(num):
-    return len(spellout(num).replace(' ', ''))
+    number_in_words = ""
+    if num > 1000 or num < 0:
+        return 'Invalid input'
+    else:
+        if num == 1000:
+            return 11
+        elif num >= 100:
+            hundreds = num / 100
+            number_in_words += "{} hundred".format(spellout(hundreds))
+        else:
+            number_in_words += "{}".format(spellout(num))
+        return len(number_in_words.replace(' ', ''))
 
-print(count_letters(1000))
+assert count_letters(1000) == 11
+assert count_letters(100) == 10
+assert count_letters(200) == 10
+assert count_letters(19) == 8
+assert count_letters(11) == 6
+assert count_letters(10) == 3
+assert count_letters(9) == 4
