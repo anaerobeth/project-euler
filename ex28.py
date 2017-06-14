387f9c9e625d5957352a581e9eedb973ae62a75b
Beth Tenorio <betsky98@gmail.com>
2017-06-14 18:08:26 -0400

    update problem 28 - compute the sum directly instead from the list

--- ex28.py
+++ ex28.py
@@ -4,22 +4,22 @@ Number spiral diagonals
 
 import itertools
 
-def diagonal_numbers(size):
-    diagonal_numbers = [1]
+def sum_of_diagonal_numbers(size):
     s = 1
+    running_total = 1
     rounds = size / 2
     # Starting from s, increment by 2 multiplied by round number
-    # and append that number to the array
+    # and update the running total
     for i in range(1, rounds+1):
         for _ in itertools.repeat(None, 4):
             # increment the step for every round
             s += i*2
-            diagonal_numbers.append(s)
-    return diagonal_numbers
+            running_total += s
+    return running_total
 
 
 if __name__ == '__main__':
-    assert sum(diagonal_numbers(5)) == 101
+    assert sum_of_diagonal_numbers(5) == 101
 
-    print(sum(diagonal_numbers(1001))) # 669171001
+    print(sum_of_diagonal_numbers(1001)) # 669171001
 
