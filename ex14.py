e1ce8eeb826bbd8d063308e16782311399321ad2
Beth Tenorio <betsky98@gmail.com>
2017-06-18 00:02:32 -0400

    complete problem 14

--- ex14.py
+++ ex14.py
@@ -4,14 +4,27 @@ n -> n/2 (n is even)
 n -> 3n + 1 (n is odd)
 Which starting number, under one million, produces the longest chain?
 """
-def next_number(num):
-    if (num % 2 == 0):
-        return num / 2
-    else:
-        return (3 * num) + 1
+def sequence(num):
+    chain = [num]
 
-for num in range(100):
-    chain = []
     while num != 1:
-        child = next_number(num)
-        num = child
+        if (num % 2 == 0):
+            num = num // 2
+        else:
+            num =  (3 * num) + 1
+        chain.append(num)
+
+    return chain
+
+
+def brute_force_solution():
+    longest_chain_length = 1
+    king = 1
+
+    for num in range(1, 1000000):
+        length = len(sequence(num))
+        if length > longest_chain_length:
+            longest_chain_length = length
+            king = num
+
+    return king # 837799
